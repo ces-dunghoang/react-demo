5f2902c4a0fe3e73f4d726187f66d375b85dea60
kaito <dung.hoang@codeenginestudio.com>
2023-12-29 15:21:58 +0700

    test : test ui change image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import coder from './coder.png';
-
+import coderTest from './—Pngtree—office figures_319491.png';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={coder} alt="logo" />
+        <img src={coderTest} alt="logo" width={300} />
         <p>
-          Welcome to demo visual testing app hehehe :v
+          Welcome to demo visual testing app 
         </p>
       </header>
     </div>
