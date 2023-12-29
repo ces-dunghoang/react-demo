7a872315f3013edd3e5d75d7a283169ae0e06e36
kaito <dung.hoang@codeenginestudio.com>
2023-12-29 13:49:01 +0700

    test : test ui change image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import coder from './coder.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={coder} alt="logo" />
         <p>
-          Edit <code>src/App.js</code>  save to.
+          Hello to demo visual testing app 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
