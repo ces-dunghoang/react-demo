e84cf9dcc3ee9ba9993df53e3f46af427dc3beab
kaito <dung.hoang@codeenginestudio.com>
2023-12-29 11:56:42 +0700

    test : test ui change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>kaito and save to reload hihi.
+          Edit <code>src/App.js</code>  save to.
         </p>
         <a
           className="App-link"
