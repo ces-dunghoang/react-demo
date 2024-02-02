e358b85391201c796e31fad33707dafc8d12403b
ces-phuongbui <phuong.bui@codeenginestudio.com>
2024-02-02 10:48:10 +0700

    ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import './App.css';
-import coder from './coder.png';
-import coderTest from './—Pngtree—office figures_319491.png';
+import "./App.css";
+import coder from "./coder.png";
+import coderTest from "./—Pngtree—office figures_319491.png";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={coderTest} alt="logo" width={300} />
-        <p>
-          Welcome to demo visual testing app sprint 6
-        </p>
+        <p>Welcome to demo visual testing app </p>
       </header>
     </div>
   );
