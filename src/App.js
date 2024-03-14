9698aea9b9d17137e1c75f006e24202687b78ff6
ces-phuongbui <phuong.bui@codeenginestudio.com>
2024-03-14 16:06:59 +0700

    mask ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import "./App.css";
-import coder from "./coder.png";
-import coderTest from "./—Pngtree—office figures_319491.png";
+import './App.css';
+import coder from './coder.png';
+import coderTest from './—Pngtree—office figures_319491.png';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>Header</h1>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={coderTest} alt="logo" width={300} />
-        <p>Welcome to demo visual testing app </p>
-      </header>
+      <Header />
+      <div >
+        <img src={coderTest} alt="coderTest" style={{width:"400px"}} />
+        <h1>Welcome to My React App </h1>
+      </div>
     </div>
   );
 }
